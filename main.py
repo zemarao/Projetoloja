137ab892339ae1e3eed2228002499f6ba7b2c40b
zemarao <zemarao@gmail.com>
2024-11-07 21:02:29 +0000

    Usa variável de ambiente PORT para compatibilidade com Heroku

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -50,5 +51,7 @@ def relatorios():
 def configuracoes():
     return "<h2>Configurações</h2><p>Conteúdo sobre configurações.</p>"
 
+# Executa a aplicação
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
